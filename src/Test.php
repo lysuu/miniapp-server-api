d5623bad6b68390d5685f9ccb7750ec3a97931af
罗言寿 <luoyanshou@vchangyi.com>
2019-04-10 17:14:32 +0800

    项目初始化。

---
+++ src/Test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Project: miniapp-server-api
+ * User: luoys
+ * Date: 2019/4/10
+ * Time: 17:07
+ */
+
+namespace ServerApi;
+
+
+class Test
+{
+    public function __construct()
+    {
+        echo "Testing ..." . PHP_EOL;
+    }
+}
