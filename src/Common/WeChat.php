ddde7819796b192b6c3e63c8615718c33d154e08
luoyanshou <shoushoustar@sina.com>
2020-10-16 14:58:18 +0800

    feat(获取accesstoken): 获取accesstoken
    获取accesstoken

--- src/Common/WeChat.php
+++ src/Common/WeChat.php
@@ -14,6 +14,19 @@ class WeChat
      * 小程序服务端接口域名
      * @var string
      */
-    protected $wxApiHost = 'https://api.weixin.qq.com/cgi-bin/';
+    protected $wxApiHost = 'https://api.weixin.qq.com';
+
+    /**
+     * @param string $uri
+     * @param string $method
+     * @param array $options
+     * @return string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    protected function Request(string $uri, string $method = 'POST', array $options = [])
+    {
+        return HttpRequest::getInstance()->Request($uri, $method, $options)
+            ->getBody()->getContents();
+    }
 
 }
--- src/Token.php
+++ src/Token.php
@@ -8,15 +8,20 @@
 
 namespace ServerApi;
 
-use ServerApi\Common\HttpRequest;
 use ServerApi\Common\WeChat;
 
-class GetAccessToken extends WeChat
+/**
+ * 获取token相关接口
+ *
+ * Class Token
+ * @package ServerApi
+ */
+class Token extends WeChat
 {
     /**
      * 获取access_token接口地址
      */
-    const GET_ACCESS_TOKEN_URL = '%stoken?appid=%s&grant_type=%s&secret=%s';
+    const GET_ACCESS_TOKEN_URL = '%s/cgi-bin/token?appid=%s&grant_type=%s&secret=%s';
 
     /**
      * @param array $options
@@ -25,16 +30,13 @@ class GetAccessToken extends WeChat
      */
     public function getAccessToken(array $options)
     {
-        if (empty($options['appid']) || empty($options['secret'])) {
-            die('Appid或Secret配置错误');
-        }
         $options['grant_type'] = $options['grant_type'] ?? 'client_credential';
         ksort($options);
         array_unshift($options, $this->wxApiHost);
         $uri = vsprintf(self::GET_ACCESS_TOKEN_URL, $options);
+        array_shift($options);
 
-        $response = HttpRequest::getInstance()->Request($uri, 'GET');
-        return $response->getBody()->getContents();
+        return $this->Request($uri, 'GET', $options);
     }
 
 }
